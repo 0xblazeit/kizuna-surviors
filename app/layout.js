818e0e9dadfa1b66e13a8c5c6c60ae72c38b6cda
Leche <lechedeburro420@gmail.com>
2024-12-01 23:16:36 -0500

    d

--- app/layout.js
+++ app/layout.js
@@ -1,24 +1,11 @@
 import localFont from "next/font/local";
-import { VT323 } from 'next/font/google'
 import "./globals.css";
 
-const vt323 = VT323({
-  weight: '400',
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-vt323',
+const vt323 = localFont({
+  src: "./fonts/VT323-Regular.ttf",
+  variable: "--font-vt323",
+  weight: "400",
 })
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Kizuna Survivors",
@@ -32,7 +19,7 @@ export default function RootLayout({ children }) {
         <script src="https://cdn.jsdelivr.net/npm/phaser@3.87.0/dist/phaser.min.js"></script>
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${vt323.variable} antialiased`}
+        className={`${vt323.variable} antialiased`}
       >
         {children}
       </body>
