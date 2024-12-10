6d9d56fba6c6f12cd5305e28ffa51cbe41542a08
Leche <lechedeburro420@gmail.com>
2024-12-09 21:11:41 -0500

    clean up

--- app/components/Game.js
+++ app/components/Game.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useRef } from "react";
+import MenuScene from "./MenuScene";
 import MainPlayer from "../game/entities/MainPlayer";
 import EnemyBasic from "../game/entities/EnemyBasic";
 import EnemyAdvanced from "../game/entities/EnemyAdvanced";
@@ -41,92 +42,6 @@ const ENEMY_EPIC_SPRITES = [
   "enemy-epic-five",
   "enemy-epic-six",
 ];
-
-const MenuScene = {
-  key: "MenuScene",
-  create: function () {
-    const { width, height } = this.scale;
-
-    // Create a simple background
-    const background = this.add.graphics();
-    background.fillGradientStyle(0x000033, 0x000033, 0x000066, 0x000066, 1);
-    background.fillRect(0, 0, width, height);
-
-    // Add title text
-    this.add
-      .text(width / 2, height / 3, "KIZUNA\nSURVIVORS", {
-        fontFamily: "VT323",
-        fontSize: "64px",
-        color: "#ffffff",
-        align: "center",
-        stroke: "#000000",
-        strokeThickness: 4,
-      })
-      .setOrigin(0.5);
-
-    // Add subtitle text
-    this.add
-      .text(width / 2, height / 3 + 80, "collect gold, survive, become strong", {
-        fontFamily: "VT323",
-        fontSize: "24px",
-        color: "#ffffff",
-        align: "center",
-        stroke: "#000000",
-        strokeThickness: 2,
-      })
-      .setOrigin(0.5);
-
-    // Add controls text
-    this.add
-      .text(width / 2, height / 3 + 140, "Controls: Arrow Keys or WASD", {
-        fontFamily: "VT323",
-        fontSize: "24px",
-        color: "#ffffff",
-        align: "center",
-        stroke: "#000000",
-        strokeThickness: 2,
-      })
-      .setOrigin(0.5);
-
-    // Add start text
-    const startText = this.add
-      .text(width / 2, height * 0.75, "Click or Press Movement Keys to Start", {
-        fontFamily: "VT323",
-        fontSize: "32px",
-        color: "#ffffff",
-      })
-      .setOrigin(0.5);
-
-    // Add blinking effect
-    this.tweens.add({
-      targets: startText,
-      alpha: 0,
-      duration: 1200,
-      ease: "Sine.easeInOut",
-      yoyo: true,
-      repeat: -1,
-      hold: 400,
-    });
-
-    // Handle click
-    this.input.on("pointerdown", () => {
-      this.scene.start("GameScene");
-    });
-
-    // Handle keyboard input
-    const startGame = () => this.scene.start("GameScene");
-
-    // Add key listeners
-    this.input.keyboard.addKey("W").on("down", startGame);
-    this.input.keyboard.addKey("A").on("down", startGame);
-    this.input.keyboard.addKey("S").on("down", startGame);
-    this.input.keyboard.addKey("D").on("down", startGame);
-    this.input.keyboard.addKey("UP").on("down", startGame);
-    this.input.keyboard.addKey("LEFT").on("down", startGame);
-    this.input.keyboard.addKey("DOWN").on("down", startGame);
-    this.input.keyboard.addKey("RIGHT").on("down", startGame);
-  },
-};
 
 const UpgradeMenuScene = Phaser.Class({
   Extends: Phaser.Scene,
