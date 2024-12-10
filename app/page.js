b65cc27b10730278b23296fe6e09ccc3e27ed340
Leche <lechedeburro420@gmail.com>
2024-12-10 17:57:35 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,18 +1,19 @@
-'use client';
+"use client";
 
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
 // Dynamically import the game component to avoid SSR issues
-const Game = dynamic(() => 
-  new Promise((resolve) => {
-    // Force a minimum delay of 2.20 seconds
-    setTimeout(() => {
-      resolve(import('./components/Game'));
-    }, 2200);
-  }), 
-  { 
+const Game = dynamic(
+  () =>
+    new Promise((resolve) => {
+      // Force a minimum delay of 2.20 seconds
+      setTimeout(() => {
+        resolve(import("./components/Game"));
+      }, 2200);
+    }),
+  {
     ssr: false,
-    loading: () => <Loading />
+    loading: () => <Loading />,
   }
 );
 
@@ -21,11 +22,14 @@ export default function Home() {
     <main className="flex justify-center items-center bg-transparent min-h-fit">
       <div className="p-8 mx-auto max-w-2xl">
         <div className="p-8 mt-8 rounded-xl shadow-lg backdrop-blur-xs bg-black/40">
-          <h1 className="mb-4 text-3xl font-bold tracking-widest text-white">Welcome to ShapeCraft Survivors</h1>
-          <p className="tracking-tightest text-slate-200">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          <h1 className="mb-4 text-3xl font-bold tracking-widest text-white">
+            Welcome to ShapeCraft Survivors
+          </h1>
+          <p className="text-xl tracking-tight text-slate-200">
+            2D rouguelike survival game where players unleash chaos in a relentless bullet firestorm to fend off endless waves of enemies while upgrading weapons used from on-chain assets living on Craft L2.
+          </p>
+          <p className="mt-2 text-xl tracking-tightest text-slate-200 text-semibold">
+            Surive the horde. Forge weapons. Climb the ranks.
           </p>
         </div>
       </div>
