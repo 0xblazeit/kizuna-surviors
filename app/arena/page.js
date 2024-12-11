727cd721c428ce4bc7ee65580647205c589e327a
Leche <lechedeburro420@gmail.com>
2024-12-11 00:37:23 -0500

    Update page.js

--- app/arena/page.js
+++ app/arena/page.js
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import { Arena } from "../components/Arena";
 
 export const metadata = {
-  title: "ShapeCraft Surviors Areana",
+  title: "ShapeCraft Surviors arena",
   description: "Prepare for Battle",
 };
 
