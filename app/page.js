853bd3154c4556a18394ed5cb8f8f065f8f4bc8a
leche <96605480+leche64@users.noreply.github.com>
2024-12-12 05:16:09 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -23,11 +23,14 @@ export default function Home() {
     <main className="flex justify-center items-center bg-transparent min-h-fit">
       <div className="p-8 mx-auto max-w-2xl">
         <div className="p-8 mt-8 rounded-xl shadow-lg backdrop-blur-xs bg-black/40">
-          <h1 className="mb-4 text-3xl font-bold tracking-widest text-white">
-            Welcome to ShapeCraft Survivors
-          </h1>
+          <h1 className="mb-4 text-3xl font-bold tracking-widest text-white">Welcome to ShapeCraft Survivors</h1>
           <p className="text-xl tracking-tight text-slate-200">
-            2D rouguelike survival game where players unleash chaos in a relentless bullet firestorm to fend off endless waves of enemies while upgrading weapons used from on-chain assets living on Craft L2.
+            2D rouguelike survival game where players unleash chaos in a relentless bullet firestorm to fend off endless
+            waves of enemies while upgrading weapons used from on-chain assets living on{" "}
+            <a href="https://shape.network/" className="text-blue-400 hover:text-blue-300">
+              Shape L2
+            </a>
+            .
           </p>
           <p className="mt-2 text-xl tracking-tightest text-slate-200 text-semibold">
             Surive the horde. Forge weapons. Climb the ranks.
