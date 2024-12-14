f60953a40a19877cd2427d2a69460fdc84318737
leche <96605480+leche64@users.noreply.github.com>
2024-12-13 20:56:40 -0500

    Update Leaderboard.js

--- app/components/Leaderboard.js
+++ app/components/Leaderboard.js
@@ -78,10 +78,10 @@ function LeaderboardTable({ data }) {
         <thead className="border-b border-white/20">
           <tr>
             <th className="p-2 text-sm font-medium tracking-wider text-center uppercase text-white/70">#</th>
-            <th className="p-2 text-sm font-medium tracking-wider text-left uppercase text-white/70">Player</th>
+            <th className="p-2 text-sm font-medium tracking-wider text-center uppercase text-white/70">Player</th>
             <th className="p-2 text-sm font-medium tracking-wider text-right uppercase text-white/70">Gold</th>
-            <th className="p-2 text-sm font-medium tracking-wider text-right uppercase text-white/70">Kills</th>
-            <th className="p-2 text-sm font-medium tracking-wider text-right uppercase text-white/70">Wave</th>
+            <th className="p-2 text-sm font-medium tracking-wider text-center uppercase text-white/70">Kills</th>
+            <th className="p-2 text-sm font-medium tracking-wider text-center uppercase text-white/70">Wave</th>
             <th className="p-2 text-sm font-medium tracking-wider text-right uppercase text-white/70">Time</th>
           </tr>
         </thead>
