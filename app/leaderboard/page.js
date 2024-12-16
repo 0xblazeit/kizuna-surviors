83afb3556642ce838571363adcdb538dc5981503
leche <96605480+leche64@users.noreply.github.com>
2024-12-15 23:40:15 -0500

    d

--- app/leaderboard/page.js
+++
@@ -1,9 +0,0 @@
-import Leaderboard from "../components/Leaderboard";
-
-export default function LeaderboardPage() {
-  return (
-    <main className="min-h-screen">
-      <Leaderboard />
-    </main>
-  );
-}
